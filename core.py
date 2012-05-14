d1ffc3dcc2e500c95821619a5b49fd99fec906ca
ch3sh1r <ch3sh1r@ya.ru>
2012-05-14 10:15:10 +0400

    Jacobi symbol and some styleing.

--- core.py
+++ core.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python
 #-*-coding:utf-8-*-
 
-russian = ["а", "б", "в", "г", "д", "е", "ж", "з",
+__rus__ = ["а", "б", "в", "г", "д", "е", "ж", "з",
            "и", "к", "л", "м", "н", "о", "п", "р",
            "с", "т", "у", "ф", "х", "ц", "ч", "ш",
            "щ", "ь", "ъ", "ы", "э", "ю", "я"]
-RUSSIAN = ["А", "Б", "В", "Г", "Д", "Е", "Ж", "З",
+__RUS__ = ["А", "Б", "В", "Г", "Д", "Е", "Ж", "З",
            "И", "К", "Л", "М", "Н", "О", "П", "Р",
            "С", "Т", "У", "Ф", "Х", "Ц", "Ч", "Ш",
            "Щ", "Ь", "Ъ", "Ы", "Э", "Ю", "Я"]
-english = [chr(x) for x in range(97, 123)]
-ENGLISH = [chr(x) for x in range(65, 91)]
+__eng__ = [chr(x) for x in range(97, 123)]
+__ENG__ = [chr(x) for x in range(65, 91)]
 
 def statistic(encrypted_message):
     '''
@@ -44,6 +44,29 @@ def totient(n):
         pos -= 1
     return tot
 
+def jacobi(a,n):
+    """
+    Jacobi symbol: (a|n).
+    """
+    if a == 0:
+        return 0
+    if a == 1:
+        return 1
+    if a == 2:
+        n8 = n % 8
+        if n8 == 3 or n8 == 5:
+            return -1
+        else:
+            return 1
+    if a%2 == 0:
+        return jacobi(2,n) * jacobi(a//2,n)
+    if a >= n:
+        return jacobi(a%n,n)
+    if a%4 == 3 and n%4 == 3:
+        return -jacobi(n,a)
+    else:
+        return jacobi(n,a)
+
 def negative(a, m):
     """
     Negative element for element a in field Z_m.
@@ -61,7 +84,6 @@ def _negative(a, m):
     """
     return a ** (totient(m) - 1) % m
 
-
 def _ord(char, alphabet = []):
     '''
     Returns position in the natural alphabet.
@@ -70,14 +92,14 @@ def _ord(char, alphabet = []):
     if alphabet != []:
         return alphabet.index(char)
     else:
-        if char in english:
-            return english.index(char)
-        elif char in ENGLISH:
-            return ENGLISH.index(char)
-        elif char in russian:
-            return russian.index(char)
-        elif char in RUSSIAN:
-            return RUSSIAN.index(char)
+        if char in __eng__:
+            return __eng__.index(char)
+        elif char in __ENG__:
+            return __ENG__.index(char)
+        elif char in __rus__:
+            return __rus__.index(char)
+        elif char in __RUS__:
+            return __RUS__.index(char)
         else:
             print "[ERROR] Aliens char detected! it is <%d>." % char
             return 0
@@ -87,11 +109,10 @@ def _chr(n, lang):
     Negative for _ord().
     '''
     if lang == 'en':
-        return ENGLISH[n-1]
-    elif lang == 'en':
-        return RUSSIAN[n]
+        return __eng__[n-1]
+    elif lang == 'ru':
+        return __rus__[n]
     else:
         print "[ERROR] No char for %d position found!" % n
         return 0
-
 
