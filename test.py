2ad3773ac8b61aac48b40ec5b98d9dfe9152b78b
Alexey Bednyakov <ch3sh1r@ya.ru>
2012-12-02 23:14:26 +0400

    Fix: Files now closed properly and Caesar cipher tests.

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 #-*-coding:utf-8-*-
 
-from cipher import affine, core
 import unittest
+from cipher import caesar, affine, core
 
 class affine_test(unittest.TestCase):
     def test_affine_encrypt(self):
@@ -16,15 +16,30 @@ class affine_test(unittest.TestCase):
                 a.decrypt(3, 4)[:20])
 
     def test_affine_decipher(self):
-        ciphertext = open('sample/affine').readlines()
-        a = affine.Affine(ciphertext)
-        x = a.decipher()
+        ciphertext = open('sample/affine')
+        a = affine.Affine(ciphertext.readlines())
+        ciphertext.close()
+        x = a.guess()[0]
         self.assertEqual(('SAUNAISNOTKNOWNTOBEA', 8, 7), 
                 (x[0][:20], x[1], x[2]))
 
+
+class caesar_test(unittest.TestCase):
+    def test_caesar_encrypt(self):
+        a = caesar.Caesar('ATTACK AT DAWN')
+        self.assertEqual('DWWDFN DW GDZQ',
+                a.encrypt(3)[:20])
+
+    def test_caesar_decrypt(self):
+        a = caesar.Caesar('DWWDFNDWGDZQ')
+        self.assertEqual('ATTACKATDAWN',
+                a.decrypt(3)[:20])
+
+
 class core_test(unittest.TestCase):
-    ciphertext = open('sample/affine').readlines()
-    a = core.Cipher(ciphertext)
+    ciphertext = open('sample/affine')
+    a = core.Cipher(ciphertext.readlines())
+    ciphertext.close()
 
     def test_sampler(self):
         self.assertEqual('BHJUHNBULSVULRUSLYXH',
