d501624f31fbf72ab82353f6c7247b328bfec094
Alexey Bednyakov <ch3sh1r@ya.ru>
2013-04-13 22:44:55 +0400

    'Attack at dawn'-driven tests.

--- test.py
+++ test.py
@@ -5,72 +5,49 @@ import unittest
 from cipher import caesar, affine, core
 
 class affine_test(unittest.TestCase):
-    ct = open('sample/affine')
-    a = core.Cipher(ct.readlines())
-    ct.close()
-
     def test_affine_encrypt(self):
         a = affine.Affine('ATTACK AT DAWN')
-        self.assertEqual('EJJEKIEJNESR',
-                a.encrypt(3, 4)[:20])
+        self.assertEqual('EJJEKI EJ NESR', a.encrypt(3, 4))
 
     def test_affine_decrypt(self):
-        a = affine.Affine('EJJEKIEJNESR')
-        self.assertEqual('ATTACKATDAWN',
-                a.decrypt(3, 4)[:20])
+        a = affine.Affine('EJJEKI EJ NESR')
+        self.assertEqual('ATTACK AT DAWN', a.decrypt(3, 4))
 
     def test_affine_decipher(self):
-        ciphertext = open('sample/affine')
-        a = affine.Affine(ciphertext.readlines())
-        ciphertext.close()
+        a = affine.Affine('EJJEKI EJ NESR')
         x = a.guess()[0]
-        self.assertEqual(('SAUNAISNOTKNOWNTOBEA', 8, 7), 
-                (x[0][:20], x[1], x[2]))
+        self.assertEqual(('ATTACK AT DAWN', 3, 4), (x[0], x[1], x[2]))
 
 
 class caesar_test(unittest.TestCase):
-    ct = open('sample/affine')
-    a = core.Cipher(ct.readlines())
-    ct.close()
-
     def test_caesar_encrypt(self):
         a = caesar.Caesar('ATTACK AT DAWN')
-        self.assertEqual('DWWDFN DW GDZQ',
-                a.encrypt(3)[:20])
+        self.assertEqual('DWWDFN DW GDZQ', a.encrypt(3))
 
     def test_caesar_decrypt(self):
         a = caesar.Caesar('DWWDFNDWGDZQ')
-        self.assertEqual('ATTACKATDAWN',
-                a.decrypt(3)[:20])
+        self.assertEqual('ATTACKATDAWN', a.decrypt(3))
 
 
 class core_test(unittest.TestCase):
-    ct = open('sample/affine')
-    a = core.Cipher(ct.readlines())
-    ct.close()
-
     def test_sampler(self):
-        sample = 'BHJUHNBULSVULRUSLYXHONUUNBWNUAXUSNLDYJSSWXRLKGNBONUUNBWSWXKXHKXDHUZDLKXBHJUHBNUONUMHUGSWHUXMBXRWXKXLUXBHJUHCXKXAXKZSWKXXLKOLJKCXLCMXONUUBVULRRWHSHBHJUHNBXMBXRWXKXNOZLJBXXHBNFUBHJUHLUSWXGLLKZLJPHUULSYXBJKXSWHSSWXKXNBHBHJUHYXWNUGSWXGLLK'
-        self.assertEqual(sample, self.a.sample)
+        a = core.Cipher('Attack at dawn, people!!!1')
+        sample = 'ATTACKATDAWNPEOPLE'
+        self.assertEqual(sample, a.sample)
 
     def test_representation(self):
+        a = core.Cipher('Attack at dawn, people!!!1')
         reprsentation = '''
-BHJUH NBULS VULRU SLYXH ONUUN BWNUA XUSNL DYJSS WXRLK GNBON UUNBW
-SWXKX HKXDH UZDLK XBHJU HBNUO NUMHU GSWHU XMBXR WXKXL UXBHJ UHCXK
-XAXKZ SWKXX LKOLJ KCXLC MXONU UBVUL RRWHS HBHJU HNBXM BXRWX KXNOZ
-LJBXX HBNFU BHJUH LUSWX GLLKZ LJPHU ULSYX BJKXS WHSSW XKXNB HBHJU
-HYXWN UGSWX GLLK'''
-        self.assertEqual(reprsentation, self.a.__repr__())
+ATTAC KATDA WNPEO PLE'''
+        self.assertEqual(reprsentation, a.__repr__())
 
     def test_statistic(self):
+        a = core.Cipher('Attack at dawn, people!!!1')
         statistic = [
-                ('X', 32), ('U', 29), ('H', 23), ('B', 19), 
-                ('L', 19), ('N', 16), ('K', 15), ('S', 15), 
-                ('W', 14), ('J', 11), ('O', 6), ('R', 6), 
-                ('G', 5), ('M', 4), ('Y', 4), ('Z', 4), 
-                ('C', 3), ('D', 3), ('A', 2), ('V', 2), 
-                ('F', 1), ('P', 1) ]
-        self.assertEqual(statistic, self.a.statistic)
+            ('A', 4), ('T', 3), ('E', 2), ('P', 2), 
+            ('C', 1), ('D', 1), ('K', 1), ('L', 1), 
+            ('O', 1), ('N', 1), ('W', 1) ]
+        self.assertEqual(statistic, a.statistic)
 
 
 if __name__ == '__main__':
