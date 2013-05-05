e9d2567c52267f80b63e3bf959a5c80d7d565459
Alexey Bednyakov <ch3sh1r@ya.ru>
2013-05-05 18:45:32 +0400

    Build files.

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+setup(
+    name='cipher',
+    version='0.0.1',
+    author=u'Alexey Bednyakov',
+    author_email='ch3sh1r@ya.ru',
+    packages=['cipher'], 
+    url='https://github.com/ch3sh1r/cryptanalysis',
+    license='GPLv2',
+    description='Do some reasearch on cryptoanalysis', 
+    long_description=open('README.md').read(),
+)
