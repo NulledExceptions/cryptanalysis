b851d8182d6b6d48cf67060275b8bd3626d999d3
Alexey Bednyakov <ch3sh1r@ya.ru>
2013-06-15 22:45:50 +0400

    Next step for clean setup.

--- setup.py
+++ setup.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from distutils.core import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
-setup(
-    name='cipher',
-    version='0.0.1',
-    author=u'Alexey Bednyakov',
-    author_email='ch3sh1r@ya.ru',
-    packages=['cipher'], 
-    url='https://github.com/ch3sh1r/cryptanalysis',
-    license='GPLv2',
-    description='Do some reasearch on cryptoanalysis', 
-    long_description=open('README.md').read(),
-)
+config = {
+    'name': 'pycry',
+    'version': '0.0.1',
+    'author': 'Alexey Bednyakov',
+    'download_url': 'https://github.com/ch3sh1r/cryptanalysis',
+    'author_email': 'ch3sh1r@ya.ru',
+    'install_requires': [],
+    'packages': ['pycry'], 
+    'scripts': [],
+    'license': 'GPLv2',
+    'description': 'Do some reasearch on cryptoanalysis', 
+    'long_description': open('README.md').read(),
+}
+
+setup(**config)
